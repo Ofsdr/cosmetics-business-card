6a321387c685f4bb959845ff1e9b27cd2c1d95c3
OFsdr <Ofsdr@proton.me>
2026-01-21 22:14:22 -0500

    navbar -- design and function

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 document.querySelector("#ChangeBack").addEventListener("click", function () {
     document.body.classList.toggle('gray-background')
 })
+
+const logo = document.querySelector('.navbar-brand');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        logo.classList.add('scrolled');
+    } else {
+        logo.classList.remove('scrolled');
+    }
+});
